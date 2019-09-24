8bbdfb0db968cc84f5c8df988b14634ab74f062f
chrismgonzalez <cmgonza89@gmail.com>
2019-09-24 13:28:16 -0500

    finished app, added build folder

---
+++ public/precache-manifest.1d0f571d9edb0618505e7df8411d0b13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d9ef2c60fd464c13f639c4e6f5a036",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f27cf2abca2f745d53e7",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "2497c356751b87d9c800",
+    "url": "/static/js/2.206596d3.chunk.js"
+  },
+  {
+    "revision": "f27cf2abca2f745d53e7",
+    "url": "/static/js/main.e7f78742.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  }
+]);
